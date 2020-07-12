e490408fe388e1a6a130995ed1f2d815a434082b
Dominion <domee.ubah@gmail.com>
2020-07-12 18:32:13 +0100

    completed UI designs for navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="app">
+      <Navbar />
     </div>
   );
 }
